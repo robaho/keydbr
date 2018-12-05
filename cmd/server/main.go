db90ac774bcf7c7f6449c21d4e01ed696bf1216e
robaho <rengels@ix.netcom.com>
2018-12-05 12:15:49 -0600

    add interceptor to log stream connections

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	pb "github.com/robaho/keydbr/internal/proto"
 	"github.com/robaho/keydbr/server"
 	"google.golang.org/grpc"
@@ -20,11 +21,23 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+
+	s := grpc.NewServer(grpc.StreamInterceptor(streamInterceptor))
 	pb.RegisterKeydbServer(s, server.NewServer(*dbpath))
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
+	fmt.Println("listening on ", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func streamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Printf("stream opened - Context:%s\n", ss.Context())
+
+	err := handler(srv, ss)
+
+	log.Printf("stream closed - Context:%s\n", ss.Context())
+
+	return err
 }
